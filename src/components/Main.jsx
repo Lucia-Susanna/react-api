b7f28474a3724e67f1d0310673bbe1646265514c
Lucia-Susanna <lucia_susanna@hotmail.it>
2025-01-22 16:22:38 +0100

    feat: stampa card dei post

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,61 @@
+import axios from "axios"
+import { useState, useEffect } from "react"
+
 const Main = () => {
+
+  const apiUrl = 'http://localhost:3000'
+  const [postsList, setPostsList] = useState([])
+
+  const fetchPosts = () => {
+
+    axios.get(`${apiUrl}/posts`)
+      .then(res => {
+        // console.log(res.data);
+        setPostsList(res.data)
+      })
+      .catch(error => {
+        console.error(error)
+      });
+  }
+
+  useEffect(() => {
+    fetchPosts()
+  }, [])
+
+  console.log(postsList);
+
+
   return (
     <main>
-      ciao
+      <div className="container">
+        <h3 className="my-3 text-center">
+          Ricette salvate
+        </h3>
+        <div className="d-flex flex-wrap">
+
+          {postsList.map(post => (
+
+            <div className="col-12 col-md-4">
+              <div className="card p-3">
+                <img src={post.image} alt="" />
+                <h5>
+                  {post.title}
+                </h5>
+                <p>
+                  {post.content}
+                </p>
+                <p>
+                  {post.tags.join(', ')}
+                </p>
+              </div>
+            </div>
+
+
+          ))}
+
+        </div>
+      </div>
+
     </main>
   )
 }
